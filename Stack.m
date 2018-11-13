57f4acc755f99fd6aa20b8dd19f38fbe22c92be2
Solvi Mar Magnusson <solvimar@gmail.com>
2018-11-13 16:25:48 -0500

    Update Stack.m
    Uppdated stack.m

--- Stack.m
+++ Stack.m
@@ -2,17 +2,23 @@
 % Methods:
 %   push(data) - Pushes data ontop of the stack
 %   peek() - peeks at the top data of the stack, does not remove from stack
+%       peak returns -1 if the stack is empty
+%       This is useful for the peg object
 %   pop() - pops the top node of the stack and returns the node value.
+%   itemArray() - return all items in the stack as array
+%   variable "size" holds how many items are in the stack
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 classdef Stack<handle
    properties
        node
+       size
    end
    methods
        %constructor
        function stack = Stack
            stack.node = Node();
+           stack.size = 0;
        end
        % push implementation for a Stack data structure
        % Push adds node with value to the top of the stack
@@ -31,6 +37,7 @@ classdef Stack<handle
               %stack.node
               stack.node.setNodeValue(data); 
           end
+          stack.size = stack.size + 1;
        end
        % peek implementation for a Stack data structure
        % peek returns the top value currently on the stack
@@ -38,7 +45,8 @@ classdef Stack<handle
            if ~isempty(stack.node.getNodeValue())
               value = stack.node.getNodeValue();
            else
-               error("can't peek, stack is empty")
+               value = -1;
+               %error("can't peek, stack is empty")
            end
        end
        % pop implementation for a Stack data structure
@@ -53,13 +61,23 @@ classdef Stack<handle
               if isempty(stack.node)
                   stack.node = Node()
               end
+              stack.size = stack.size - 1;
           else
               % if stack is empty
               %display error message
               stack.node = Node()
               error("Can't pop off stack, stack is empty");
           end
-          
        end
+       function value = itemArray(stack)
+           value = [];
+           currentStack = stack.node;
+           for i = 1 : stack.size
+               nodeValue = currentStack.getNodeValue();
+               value(i) = nodeValue;
+               currentStack = currentStack.getNextNode();
+           end
+       end
+       
    end
 end
